ed9d81e1c64bd06fc9d3e019e0f43acc24109e3b
Proso Philiac <prosophiliac@gmail.com>
2015-10-09 10:06:44 +0530

    Error Message

--- git/maintenance.sh
+++ git/maintenance.sh
@@ -1,3 +1,13 @@
+#Fixes 
+#fatal: early EOF
+#fatal: index-pack failed
+#
+#Please add any other error messages that are solved by this above. 
+
+
+
+
+
 # Verifies the connectivity and validity of the objects in the database
 git fsck --unreachable
 
