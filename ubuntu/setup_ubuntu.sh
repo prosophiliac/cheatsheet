c1504462d41c5e1bb777705f465e835641cdeadb
Sougata <sougat818@gmail.com>
2015-11-11 21:10:16 +0000

    Fixed Bug on vim plugin install

--- ubuntu/setup_ubuntu.sh
+++ ubuntu/setup_ubuntu.sh
@@ -35,8 +35,8 @@ function aptupdate {
 function vim {
     sudo apt-get install -y vim
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
-    wget https://raw.githubusercontent.com/sougat818/cheatsheet/master/vim/.vimrc -O $HOME
-    vim +PluginInstall +qall
+    wget https://raw.githubusercontent.com/sougat818/cheatsheet/master/vim/.vimrc -O $HOME/.vimrc
+    command vim +PluginInstall +qall
 }
 
 #aptupgrade
